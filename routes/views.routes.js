61ff212cb02183ab164dc3e5ae6fd2fa144048aa
QobiljonYuldashev <qobiljonyuldashev044>
2025-05-19 21:12:00 +0500

    done dict,author,topic and login

--- routes/views.routes.js
+++ routes/views.routes.js
@@ -1,5 +1,7 @@
 const { createViewPage } = require("../helpers/create_view_page");
-
+const topicsModel = require("../schemas/Topic");
+const authorsModel = require("../schemas/Author");
+const dictModel = require("../schemas/Dict");
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
@@ -9,24 +11,32 @@ router.get("/", (req, res) => {
   });
 });
 
-router.get("/dictionary", (req, res) => {
+router.get("/dictionary", async (req, res) => {
+  let dict = await dictModel.find().lean();
+
   res.render(createViewPage("dictionary"), {
     title: "Lug'atlar sahifa",
     isDictionary: true,
+    dict,
   });
 });
 
-router.get("/authors", (req, res) => {
+router.get("/authors", async (req, res) => {
+  let authors = await authorsModel.find().lean();
+  console.log(authors);
   res.render(createViewPage("authors"), {
     title: "Mualliflar sahifa",
     isAuthors: true,
+    authors,
   });
 });
 
-router.get("/topics", (req, res) => {
+router.get("/topics", async (req, res) => {
+  let topics = await topicsModel.find().lean();
   res.render(createViewPage("topics"), {
-    title: "Maqolalar sahifa",
     isTopics: true,
+    title: "Mavzular sahifasi",
+    topics,
   });
 });
 
